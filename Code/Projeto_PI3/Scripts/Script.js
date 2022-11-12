81c3e7d4b256af7b99065c4f3ac360d33c94b7cd
T768544 <paula.melo@santander.com.br>
2022-11-12 19:34:17 -0300

    Adjusts Auth Login

--- Code/Projeto_PI3/Scripts/Script.js
+++ Code/Projeto_PI3/Scripts/Script.js
@@ -13,32 +13,24 @@ function frameLooper () {
 }
 frameLooper();
 
-function onSignIn(googleUser){
-  var profile = googleUser.getBasicProfile;
-  startLogin();
+var startApp = function() {
+  gapi.load('auth2', function(){
+    auth2 = gapi.auth2.init({
+      client_id: 'client',
+      cookiepolicy: 'single_host_origin',
+      scope: 'profile email'
+    });
+    auth2.attachClickHandler(document.getElementById('login'), {}, onSuccess, onFailure);
+  });
+};
+
+function onSuccess(googleUser) {
+    var profile = googleUser.getBasicProfile();
+
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log("ID Token: " + id_token);
 }
 
-var startLogin = function(){
-  gapi.load('auth2', function(){
-    auth2 = gapi.auth2.init(
-      {
-        client_id: 'client_id',
-        cokkiepolicy: 'single_host_origin',
-        scope: 'profile email'
-      });
-      auth2.attachClickHandler(document.getElementById('llogin'),{}, onSuccess, onFailure);
-  });
-
-  function onSuccess(googleUser){
-        var profile = response.getBasicProfile();
-        var userID = perfil.getId();
-        var userName = perfil.getName();
-        var userEmail = perfil.getEmail();
-        var userPicture = perfil.getImageUrl();
-
-  }
-
-  function onFailure(error){
+function onFailure(error) {
     console.log(error);
-  }  
 }
